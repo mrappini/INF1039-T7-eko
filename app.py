87017600dca1b8916b0997148ed711b650ac6b73
Mario Rappini <mariomendesrappini@gmail.com>
2025-11-03 23:26:52 -0300

    ainda mais mudanças
    muita coisa pra fazer ainda, e muita coisa confusa. ainda colocarei comentários pra todos saberem o que é o que. nessa mudança, basicamente, a maioria dos botões funciona. separei os botoes de entrar e de cadastro por enquanto. preciso mudar o nome das classes, variaveis, etc, aportuguesar oq for possível.

--- app.py
+++ app.py
@@ -2,13 +2,29 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route('/inicio')
+@app.route('/')
 def homepage():
     return render_template('homepage.html')
 
 @app.route('/avaliacao')
 def avaliacao():
+
     return render_template('avaliacao.html')
+
+@app.route('/cadastro')
+def cadastro():
+
+    return render_template('cadastro.html')
+
+
+@app.route('/login')
+def login():
+
+    return render_template('login.html')
+
+
+
 
 if __name__ == '__main__':
     app.run(debug=True)
+
